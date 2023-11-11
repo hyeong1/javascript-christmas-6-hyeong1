fecd2b2e605450f9f00c00ef10d1e65ccd6f58a9
hyeong1 <mi75625@gmail.com>
2023-11-11 23:56:42 +0900

    style: 안쓰는 import 삭제

--- src/InputView.js
+++ src/InputView.js
@@ -1,14 +1,12 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 const InputView = {
   printStart() {
-    MissionUtils.Console.print(
-      "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
-    );
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
   },
 
   async readDate() {
-    const input = await MissionUtils.Console.readLineAsync(
+    const input = await Console.readLineAsync(
       "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
     );
     // ...
