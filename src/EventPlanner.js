df56687531bdff9f5715618cab1b7b3dd9684cde
hyeong1 <mi75625@gmail.com>
2023-11-11 23:54:56 +0900

    feat: 이벤트플래너 시작문구 출력

---
+++ src/EventPlanner.js
@@ -0,0 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+import Menu from "./constant/Menu.js";
+import InputView from "./InputView.js";
+
+class EventPlanner {
+  constructor() {
+    InputView.printStart();
+  }
+}
+
+const eventPlanner = new EventPlanner();
