17be1b4b330e54c879286c29247ef73f3cad5fa3
hyeong1 <mi75625@gmail.com>
2023-11-12 00:20:03 +0900

    feat: 방문 날짜 입력 후 날짜 체크

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
+import StateCheck from "./StateCheck.js";
+
 const InputView = {
   printStart() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
@@ -9,9 +11,8 @@ const InputView = {
     const input = await Console.readLineAsync(
       "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
     );
-    // ...
+    return StateCheck.dateCheck(parseInt(input));
   },
-  // ...
 };
 
 export default InputView;
