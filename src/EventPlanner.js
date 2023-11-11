f5285f3d3ec0ce1f81596b93a4618bc719c1891d
hyeong1 <mi75625@gmail.com>
2023-11-12 00:20:43 +0900

    feat: 방문 날짜 입력값 저장

--- src/EventPlanner.js
+++ src/EventPlanner.js
@@ -1,9 +1,17 @@
 import InputView from "./InputView.js";
+import OutputView from "./OutputView.js";
 
 class EventPlanner {
+  #date = {};
   constructor() {
     InputView.printStart();
+  }
+
+  async date() {
+    this.#date = await InputView.readDate();
+    OutputView.printDate(this.#date);
   }
 }
 
 const eventPlanner = new EventPlanner();
+eventPlanner.date();
