a40e545f9a6f7b5aef39c237723273b8dd4170a9
hyeong1 <mi75625@gmail.com>
2023-11-12 00:23:59 +0900

    refator: dateCheck내에 조건문만

--- src/StateCheck.js
+++ src/StateCheck.js
@@ -1,21 +1,21 @@
-const StateCheck = {
+const DateCheck = {
   dateState: {
     isWeek: false,
     isWeekend: false,
     isStar: false,
     isDday: false,
   },
+  weekend: [1, 2, 8, 9, 15, 16, 22, 23, 29, 30],
+  star: [3, 10, 17, 24, 25, 31],
 
   dateCheck(date) {
-    const weekend = [1, 2, 8, 9, 15, 16, 22, 23, 29, 30];
-    const star = [3, 10, 17, 24, 25, 31];
-    if (!weekend.includes(date)) {
+    if (!this.weekend.includes(date)) {
       this.dateState.isWeek = true;
     }
-    if (weekend.includes(date)) {
+    if (this.weekend.includes(date)) {
       this.dateState.isWeekend = true;
     }
-    if (star.includes(date)) {
+    if (this.star.includes(date)) {
       this.dateState.isStar = true;
     }
     if (date < 26) {
@@ -25,4 +25,4 @@ const StateCheck = {
   },
 };
 
-export default StateCheck;
+export default DateCheck;
