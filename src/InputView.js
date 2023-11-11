34da782471b4e3623dd1e17f503c49480f302ef6
hyeong1 <mi75625@gmail.com>
2023-11-11 23:53:53 +0900

    feat: 시작 문구 출력

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,19 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const InputView = {
+  printStart() {
+    MissionUtils.Console.print(
+      "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
+    );
+  },
+
+  async readDate() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+    );
     // ...
-}
+  },
+  // ...
+};
+
+export default InputView;
