b3987df65915317b38763d6d1f092fb809502ccb
hyeong1 <mi75625@gmail.com>
2023-11-13 00:54:08 +0900

    feat: 할인 전 총주문 금액 확인

---
+++ src/PriceCheck.js
@@ -0,0 +1,13 @@
+import Menu from "./constant/Menu.js";
+
+const PriceCheck = {
+  priceBeforeEvent(menus) {
+    let price = 0;
+    for (const [key, value] of Object.entries(menus)) {
+      price += Menu[key] * value;
+    }
+    return price;
+  },
+};
+
+export default PriceCheck;
