2ada942bcd6f7283670595f0beeeba7bcb9b4224
hyeong1 <mi75625@gmail.com>
2023-11-13 02:05:40 +0900

    style: 금액 출력 형식 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -28,7 +28,7 @@ const OutputView = {
   printPriceBeforeEvent(menus) {
     Console.print("<할인 전 총주문 금액>");
     this.price = PriceCheck.priceBeforeEvent(menus);
-    Console.print(this.price + "원\n");
+    Console.print(this.price.toLocaleString() + "원\n");
   },
   printGetFree() {
     Console.print("<증정 메뉴>");
@@ -42,23 +42,31 @@ const OutputView = {
   printTotalEvent(date, menu) {
     Console.print("<혜택 내역>");
     if (EventCheck.isDday(date)) {
-      Console.print(`크리스마스 디데이 할인: -${EventCheck.isDday(date)}원`);
+      Console.print(
+        `크리스마스 디데이 할인: -${EventCheck.isDday(date).toLocaleString()}원`
+      );
       this.sale += EventCheck.isDday(date);
     }
     if (EventCheck.isWeek(date, menu)) {
-      Console.print(`평일 할인: -${EventCheck.isWeek(date, menu)}원`);
+      Console.print(
+        `평일 할인: -${EventCheck.isWeek(date, menu).toLocaleString()}원`
+      );
       this.sale += EventCheck.isWeek(date, menu);
     }
     if (EventCheck.isWeekend(date, menu)) {
-      Console.print(`주말 할인: -${EventCheck.isWeekend(date, menu)}원`);
+      Console.print(
+        `주말 할인: -${EventCheck.isWeekend(date, menu).toLocaleString()}원`
+      );
       this.sale += EventCheck.isWeekend(date, menu);
     }
     if (EventCheck.isStar(date)) {
-      Console.print(`특별 할인: -${EventCheck.isStar(date)}원`);
+      Console.print(
+        `특별 할인: -${EventCheck.isStar(date).toLocaleString()}원`
+      );
       this.sale += EventCheck.isStar(date);
     }
     if (EventCheck.isGetFree(this.price)) {
-      Console.print("증정 이벤트: -25000원");
+      Console.print("증정 이벤트: -25,000원");
     }
     if (
       !EventCheck.isDday(date) &&
@@ -73,15 +81,15 @@ const OutputView = {
     Console.print("");
     Console.print("<총혜택 금액>");
     if (this.sale === 0) {
-      Console.print(`${this.sale}원\n`);
+      Console.print(`${this.sale.toLocaleString()}원\n`);
     }
     if (this.sale !== 0) {
-      Console.print(`-${this.sale}원\n`);
+      Console.print(`-${this.sale.toLocaleString()}원\n`);
     }
   },
   printPriceAfterEvent() {
     Console.print("<할인 후 예상 결제 금액>");
-    Console.print(`${this.price - this.sale}원\n`);
+    Console.print(`${(this.price - this.sale).toLocaleString()}원\n`);
   },
   printBadge() {
     Console.print("<12월 이벤트 배지>");
