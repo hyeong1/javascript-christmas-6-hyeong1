a325771f9159e23188eb9b5672aca34fa4e59e84
hyeong1 <mi75625@gmail.com>
2023-11-13 01:10:39 +0900

    feat: 크리스마스 디데이 체크

--- src/EventCheck.js
+++ src/EventCheck.js
@@ -7,6 +7,14 @@ const EventCheck = {
       return false;
     }
   },
+  isDday(date) {
+    if (date[0] < 26) {
+      return 100 * (date[0] - 1) + 1000;
+    }
+    if (date[0] > 25) {
+      return false;
+    }
+  },
 };
 
 export default EventCheck;
