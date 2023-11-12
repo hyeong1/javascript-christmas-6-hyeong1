bef048d3b551ea8ea44bdf5c4f854d25f6cc066a
hyeong1 <mi75625@gmail.com>
2023-11-12 23:59:37 +0900

    feat: 주문 메뉴 입력 받기

--- src/EventPlanner.js
+++ src/EventPlanner.js
@@ -2,12 +2,14 @@ import InputView from "./InputView.js";
 
 class EventPlanner {
   #date = [];
+  #menu = {};
   constructor() {
     InputView.printStart();
   }
 
   async date() {
     this.#date = await InputView.readDate();
+    this.#menu = await InputView.readMenu();
   }
 }
 
