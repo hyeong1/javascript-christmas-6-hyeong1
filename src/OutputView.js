89c4ae8333055ea3b13cf288fe83d761755f4561
hyeong1 <mi75625@gmail.com>
2023-11-13 01:54:25 +0900

    fix: 총혜택 금액 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -60,7 +60,7 @@ const OutputView = {
   printTotalSale() {
     Console.print("");
     Console.print("<총혜택 금액>");
-    Console.print(`-${this.price}원\n`);
+    Console.print(`-${this.sale}원\n`);
   },
   printPriceAfterEvent() {
     Console.print("<할인 후 예상 결제 금액>");
