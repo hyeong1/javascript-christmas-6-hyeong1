7f0a6611cc6995649991a09501829d5c3b31dd53
hyeong1 <mi75625@gmail.com>
2023-11-12 23:58:51 +0900

    feat: 주문 메뉴 입력 받기

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
 import DateCheck from "./DateCheck.js";
+import MenuCheck from "./MenuCheck.js";
 
 const InputView = {
   printStart() {
@@ -9,9 +10,16 @@ const InputView = {
 
   async readDate() {
     const input = await Console.readLineAsync(
-      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n"
     );
     return [parseInt(input), DateCheck.dateCheck(parseInt(input))];
+  },
+
+  async readMenu() {
+    const input = await Console.readLineAsync(
+      "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n"
+    );
+    return MenuCheck.menuCheck(input);
   },
 };
 
