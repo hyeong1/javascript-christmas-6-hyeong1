724774ccf51d6e4b10d9e40939806a8c796beb78
hyeong1 <mi75625@gmail.com>
2023-11-13 00:40:08 +0900

    add: 주문 메뉴 확인 메소드 추가

--- src/MenuCheck.js
+++ src/MenuCheck.js
@@ -1,5 +1,36 @@
+import Menu from "./constant/Menu.js";
+
 const MenuCheck = {
   menuResult: {},
+  isOnlyDrink() {
+    const drinkCheck = ["제로콜라", "레드와인", "샴페인"];
+    let counter = 0;
+    for (const [key, value] of Object.entries(this.menuResult)) {
+      if (drinkCheck.includes(key)) {
+        counter++;
+      }
+      if (counter == Object.keys(this.menuResult).length) {
+        return true;
+      }
+    }
+  },
+  isExistMenu() {
+    for (const [key, value] of Object.entries(this.menuResult)) {
+      if (!Menu.hasOwnProperty(key)) {
+        return true;
+      }
+    }
+  },
+  isNum() {
+    for (const [key, value] of Object.entries(this.menuResult)) {
+      return isNaN(value);
+    }
+  },
+  typeCheck() {
+    this.isOnlyDrink();
+    this.isExistMenu();
+    this.isNum();
+  },
   menuCheck(menus) {
     const menu = menus.split(",");
     menu.forEach((menuItem) => {
