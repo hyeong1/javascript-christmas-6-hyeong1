667561f93a58a4931ef8e1512269258f193422c6
hyeong1 <mi75625@gmail.com>
2023-11-13 01:37:09 +0900

    feat: 주말 할인 확인

--- src/EventCheck.js
+++ src/EventCheck.js
@@ -34,6 +34,30 @@ const EventCheck = {
       return false;
     }
   },
+  isWeekend(date, menu) {
+    const main = [
+      "티본스테이크",
+      "바비큐립",
+      "해산물파스타",
+      "크리스마스파스타",
+    ];
+    let counter = 0;
+    if (!date[1].isWeekend) {
+      return false;
+    }
+    for (const [key, value] of Object.entries(menu)) {
+      if (main.includes(key)) {
+        counter++;
+        counter *= value;
+      }
+    }
+    if (counter !== 0) {
+      return counter * 1000;
+    }
+    if (counter === 0) {
+      return false;
+    }
+  },
 };
 
 export default EventCheck;
