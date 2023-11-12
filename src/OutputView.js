4ab970bbe2bca7a27aa80f6ee6790116203736e4
hyeong1 <mi75625@gmail.com>
2023-11-13 00:54:19 +0900

    feat: 할인 전 총주문 금액 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,11 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 
+import PriceCheck from "./PriceCheck.js";
+
 const OutputView = {
   printEvent(date, menus) {
     Console.print(
       `12월 ${date[0]}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`
     );
     this.printMenu(menus);
+    this.printPriceBeforeEvent(menus);
   },
   printMenu(menus) {
     Console.print("<주문 메뉴>");
@@ -13,8 +16,9 @@ const OutputView = {
       Console.print(`${key} ${value}개\n`);
     }
   },
-  printPriceBeforeEvent() {
+  printPriceBeforeEvent(menus) {
     Console.print("<할인 전 총주문 금액>");
+    Console.print(PriceCheck.priceBeforeEvent(menus) + "\n");
   },
 };
 
