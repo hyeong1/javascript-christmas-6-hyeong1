45b154f8b8826d59928cd492aae21228d65f62fe
hyeong1 <mi75625@gmail.com>
2023-11-15 21:17:17 +0900

    style: 테스트 이름 수정

--- __tests__/MenuCheck.js
+++ __tests__/MenuCheck.js
@@ -1,6 +1,6 @@
-import PriceCheck from "../src/checks/PriceCheck";
+import MenuCheck from "../src/checks/MenuCheck";
 
-describe("금액 체크 테스트", () => {
+describe("메뉴 체크 테스트", () => {
   test("타파스 1개, 레드와인 1개 주문하면 65,500원", () => {
     const menus = { 타파스: 1, 레드와인: 1 };
     expect(PriceCheck.priceBeforeEvent(menus)).toBe(65500);
