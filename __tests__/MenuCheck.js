72e6ea6a4faf3faf8bf52c6d8c5489809f26afd0
hyeong1 <mi75625@gmail.com>
2023-11-15 21:22:10 +0900

    test: 메뉴 체크 테스트 추가

--- __tests__/MenuCheck.js
+++ __tests__/MenuCheck.js
@@ -1,8 +1,23 @@
 import MenuCheck from "../src/checks/MenuCheck";
 
 describe("메뉴 체크 테스트", () => {
-  test("타파스 1개, 레드와인 1개 주문하면 65,500원", () => {
-    const menus = { 타파스: 1, 레드와인: 1 };
-    expect(PriceCheck.priceBeforeEvent(menus)).toBe(65500);
+  test("음료만 주문", () => {
+    MenuCheck.menuResult = { 샴페인: 1, 레드와인: 1 };
+    expect(MenuCheck.isOnlyDrink()).toBe(true);
+  });
+
+  test("메뉴에 없는 메뉴 주문", () => {
+    MenuCheck.menuResult = { 삼계탕: 1, 레드와인: 1 };
+    expect(MenuCheck.isExistMenu()).toBe(false);
+  });
+
+  test("메뉴 수량에 0 입력", () => {
+    MenuCheck.menuResult = { 샴페인: 0, 초코케이크: 1 };
+    expect(MenuCheck.isNum()).toBe(false);
+  });
+
+  test("메뉴 20개 초과 주문", () => {
+    MenuCheck.menuResult = { 초코케이크: 15, 레드와인: 7 };
+    expect(MenuCheck.isUnder20()).toBe(false);
   });
 });
